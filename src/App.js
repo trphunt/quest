1b4f3b613da55f72eeb4bc707b8cc41533da24ca
Tyler Hunt <tyler.rp.hunt@gmail.com>
2019-07-09 20:27:40 -0400

    render results on the page

--- src/App.js
+++ src/App.js
@@ -9,18 +9,53 @@ let originalArray = [
 
 export default class App extends Component {
 
-  componentDidMount() {
+  renderOriginalArray() {
+    return originalArray.map((item, index) => {
+      return (
+        <div key={'orig_item' + index}>
+          position: {item.position}, name: {item.name}
+        </div>
+      );
+    });
+  }
+
+  renderReducedArray() {
     let reducedArray = originalArray.reduce(myReducer, {});
-    console.log('originalArray:', originalArray);
-    console.log('reducedArray:', reducedArray);
-    console.log('expandedArray:', Object.entries(reducedArray).reduce(myExpander, []));
+    return Object.entries(reducedArray).map((item, index) => {
+      return (
+        <div key={'reduced_item' + index}>
+          {item[0]}: {item[1]}
+        </div>
+      );
+    });
+  }
+
+  renderExpandedArray() {
+    let reducedArray = originalArray.reduce(myReducer, {});
+    return Object.entries(reducedArray)
+      .reduce(myExpander, [])
+      .map((item, index) => {
+        return (
+          <div key={'exp_item' + index}>
+            position: {item.position}, name: {item.name}
+          </div>
+        );
+      });
   }
 
   render() {
     return (
       <div>
-        <h1>Quest</h1>
-        <h2></h2>
+        <h1>Reducer Quest</h1>
+        <hr />
+        <h2>Original Array:</h2>
+        {this.renderOriginalArray()}
+        <hr />
+        <h2>Reduced Array:</h2>
+        {this.renderReducedArray()}
+        <hr />
+        <h2>Expanded Array:</h2>
+        {this.renderExpandedArray()}
       </div>
     );
   }
